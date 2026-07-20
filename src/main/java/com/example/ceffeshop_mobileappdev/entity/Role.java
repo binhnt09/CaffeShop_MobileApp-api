6333aa74bf3bde84bbedf9fde938e88bcba3077b
HuyNguyne2004 <165890212+HuyNguyne2004@users.noreply.github.com>
2026-07-20 18:40:04 +0700

    fix

--- src/main/java/com/example/ceffeshop_mobileappdev/entity/Role.java
+++ src/main/java/com/example/ceffeshop_mobileappdev/entity/Role.java
@@ -28,5 +28,27 @@ public class Role {
     @Column(name = "Description")
     private String description;
 
+    public Integer getId() {
+        return id;
+    }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
